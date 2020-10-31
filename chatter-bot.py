8c7e2ae2876b85c0eb414466686c00b40f317ebf
Alex <alex@itdoctors.com>
2020-10-30 20:57:18 -0400

    Add events to basic actions

--- chatter-bot.py
+++ chatter-bot.py
@@ -1,1 +1,30 @@
-#Chatter Bot for Discord
+# Chatter Bot for Discord
+import discord
+
+
+# Initialize the bot here
+client = discord.Client()
+
+
+# As a rule discord require these events to be run as coroutines
+@client.event
+async def on_ready():
+    general_channel = client.get_channel(679832307066208440)
+    await general_channel.send('Hello Juniors!')
+
+
+@client.event
+async def on_disconnect():
+    cromulon_channel = client.get_channel(707682506291412993)
+    await cromulon_channel.send("Goodbye!")
+
+
+@client.event
+async def on_message(message):
+
+    if message.content == 'chatter what is your version':
+        cromulon_channel = client.get_channel(707682506291412993)
+        await cromulon_channel.send("The version is 1.0!")
+
+# Run the client on the server
+client.run("NzcxODYxODk3Nzg4MTI5Mjgx.X5ySaA.jLrUMauYqNPo9eHSYGkOUGOtcoo")
