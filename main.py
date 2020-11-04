90bd5f80ece46b65ed4d127a228ef7516e47a051
alexanderJHarris <ajharris8814@gmail.com>
2020-11-03 22:09:59 -0500

    Refactor main.py

--- main.py
+++ main.py
@@ -1,40 +1,12 @@
-# Chatter Bot for Discord
-# Written by @alexanderJHarris on github
-
-# Discord imports
-import discord
+import os
 from discord.ext import commands
+from settings import *
 
-# Python imports
-import config
-import pandas
+bot = commands.Bot(command_prefix="~")
 
+for filename in os.listdir("./cogs"):
+    if filename.endswith(".py") and filename != "__init__.py":
+        bot.load_extension(f'cogs.{filename[:-3]}')
 
-# Initialize the bot object
-client = commands.Bot(command_prefix="~")
-
-
-# As a rule discord requires any events to be run as coroutines (async functions in python)
-
-@client.command(name="version")
-async def version(context):  # Context is taking the scope of where this event is called
-
-    myEmbed = discord.Embed( title="Current Version", description="This bot is in version 1.0", color=0x98ff98)
-    myEmbed.add_field(name="Version Code: ", value="v1.0.0", inline=False)
-    myEmbed.add_field(name="Date Released:", value="November 1st, 2020", inline=False)
-    myEmbed.set_footer(text="Release version message")
-    myEmbed.set_author(name=config.author_name)
-
-    await context.message.channel.send(embed=myEmbed) #using the context param above to send the embed to requested channel
-
-@client.event
-async def on_message(message):
-
-    if message == 'send a dm':
-        await message.author.send("This is a test DM, have a good day")
-
-
-
-# Run the client
-
-client.run(config.bot_token)
+print(DISCORD_BOT_TOKEN)
+# bot.run(DISCORD_BOT_TOKEN)
