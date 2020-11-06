faf9b040ad795a2e3f623b72643e08fbdfb70d33
alexanderJHarris <ajharris8814@gmail.com>
2020-11-06 14:32:26 -0500

    Add listener for temporary voice chats

--- cogs/activities.py
+++ cogs/activities.py
@@ -48,5 +48,24 @@ class Activities(commands.Cog):
                                 print("User stopped streaming")
                                 self.current_streamers.remove(member.id)
                             break
+
+        if after.channel is not None:
+            if after.channel.name == "template":
+                channel = await create_voice_channel(after.channel.guild, f"{member.name}-conference".lower(), category_name="conference")
+
+                if channel is not None:
+                    await member.move_to(channel)
+
+        if before.channel is not None:
+            if before.channel.category.id == get_category_by_name(before.channel.guild, "conference").id:
+                print(f"{member.name} has left temporary channel")
+
+                if before.channel.name == "template":
+                    return
+
+                if len(before.channel.members) == 0:
+                    print("The channel is empty")
+                    await before.channel.delete()
+
 def setup(bot):
     bot.add_cog(Activities(bot))
