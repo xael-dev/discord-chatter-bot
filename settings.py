1d9d2486b5d62328e886b58ad0486adb6a589e22
alexanderJHarris <ajharris8814@gmail.com>
2020-11-06 03:36:54 -0500

    Add print statements for scope checking in logs

--- settings.py
+++ settings.py
@@ -3,10 +3,12 @@ import os
 DEBUG = os.getenv("DEBUG", False)
 
 if DEBUG:
+    print("**You are in a debugging environment!")
     from pathlib import Path
-    from dotenv import load_dotenv
-    env_path = Path(".") / "env.debug"
+    from dotenv import load_dotenv # For docker later on
+    env_path = Path(".") / ".env.debug"
     load_dotenv(dotenv_path=env_path)
     from settings_files.development import *
 else:
+    print("**You are in a production environment!")
     from settings_files.production import *
