06ee35997de5b5813613fd68a45578c2d08eb2a8
alexanderJHarris <ajharris8814@gmail.com>
2020-11-05 23:54:42 -0500

    Add logging to project

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from discord.ext import commands
 from settings import *
+import logging
 
 bot = commands.Bot(command_prefix="~")
 
@@ -8,5 +9,6 @@ for filename in os.listdir("./cogs"):
     if filename.endswith(".py") and filename != "__init__.py":
         bot.load_extension(f'cogs.{filename[:-3]}')
 
-print(DISCORD_BOT_TOKEN)
-# bot.run(DISCORD_BOT_TOKEN)
+logging.basicConfig(level=logging.DEBUG)
+
+bot.run(DISCORD_BOT_TOKEN)
