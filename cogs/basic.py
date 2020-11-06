396386756489a14a2890d955bb4268593d6cae85
alexanderJHarris <ajharris8814@gmail.com>
2020-11-05 23:53:01 -0500

    Add basic name command as a precursor to voice chat renaming

--- cogs/basic.py
+++ cogs/basic.py
@@ -8,5 +8,10 @@ class Basic(commands.Cog):
     async def ping(self, context):
         await context.send("pong")
 
+    @commands.command() # Make a rudimentary command to change the VC name, may not be the best way to implement atm.
+    async def name(self, context, *args):
+        print("Voice chat name has changed to" + str(args))
+        await context.send(" ".join(args))
+
 def setup(bot):
     bot.add_cog(Basic(bot))
