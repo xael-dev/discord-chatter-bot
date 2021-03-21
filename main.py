466a8d8a2df768c99ee22cab7654a97173db10be
Alex Harris <ajharris8814@gmail.com>
2021-03-21 11:30:27 -0400

    Add intents as per new discord conventions

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import os
 import logging
+import discord
 from discord.ext import commands
 from settings import *
 
-bot = commands.Bot(command_prefix="~")
+intents = discord.Intents().all()
+bot = commands.Bot(command_prefix="~", intents=intents)
 
 for filename in os.listdir("./cogs"):
     if filename.endswith(".py") and filename != "__init__.py":
         bot.load_extension(f'cogs.{filename[:-3]}')
 
-logging.basicConfig(level=logging.DEBUG)
+# logging.basicConfig(level=logging.DEBUG)
 
 bot.run(DISCORD_BOT_TOKEN)
