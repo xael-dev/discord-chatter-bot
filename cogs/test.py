234a46dfc460441dc96278dc026dd00446dc9181
Alex Harris <ajharris8814@gmail.com>
2021-03-21 11:29:34 -0400

    Change command name for posterity

--- cogs/test.py
+++ cogs/test.py
@@ -1,12 +1,14 @@
 from discord.ext import commands
 
+
 class Testing(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-    
+
     @commands.command()
-    async def hello(self, context, *args):
+    async def ban(self, context, *args):
         await context.send(','.join(args))
+
 
 def setup(bot):
     bot.add_cog(Testing(bot))
